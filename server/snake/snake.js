322259a2d6669574ede30b24ef480e5e71b24c11
sklarqui <sklarqui@purdue.edu>
2018-04-12 19:06:57 -0400

    I got clearCanvas and updateSnakeCanvas function to work

--- server/snake/snake.js
+++ server/snake/snake.js
@@ -1,16 +1,22 @@
 class SnakeGame{
+//Snake game constructor
 constructor(){
 this.snakeCanvas = document.getElementById("snakeCanvas");
  this.context =snakeCanvas.getContext("2d");
 this.context.fillRect(10,10,50,50);
-this.interval = setInterval(this.updateSnakeCanvas,20);
+this.interval = setInterval(this.updateSnakeCanvas.bind(this),20);
 }
-clearCanvas(){
-     this.context.clearRect(0, 0, this.snakeCanvas.width, this.snakeCanvas.height);
-}
+    //updates snakecanvas
 updateSnakeCanvas(){
-   clearCanvas(); 
-   this.snakeCanvas.update();
+
+    //reaches this function just doesn't do either
+   this.clearCanvas(); 
+    this.snakeCanvas.update();
+}
+//clears Snake canvas of objects
+clearCanvas(){
+    //this.snakeCanvas.style.background="blue";
+     this.context.clearRect(0, 0, this.snakeCanvas.width, this.snakeCanvas.height);
 }
 
 }
