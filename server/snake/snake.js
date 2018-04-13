0b74470365f3848d277fbf84bd6e928e15486472
sklarqui <sklarqui@purdue.edu>
2018-04-12 20:48:47 -0400

    the snake finally has a tail now

--- server/snake/snake.js
+++ server/snake/snake.js
@@ -3,8 +3,11 @@ class SnakeGame{
 constructor(){
 this.snakeCanvas = document.getElementById("snakeCanvas");
  this.context =snakeCanvas.getContext("2d");
+ //1 up,2 down,3 right, 4 left
+ this.direction=1;
+ this.food=0;
 //this.context.fillRect(10,10,50,50);
-this.interval = setInterval(this.updateSnakeCanvas.bind(this),20);
+this.interval = setInterval(this.updateSnakeCanvas.bind(this),40);
 this.x=[0];
 this.y=[0];
 this.snakeHead = {
@@ -20,25 +23,24 @@ this.snakeHead = {
 }
     //updates snakecanvas
 updateSnakeCanvas(){
-    this.snakeCanvas.style.backgroundColor="red";
-var food=0;
-console.log("herre");
-    if(food===1){
+    if(this.food===1){
 this.x.push(this.snakeHead.y);
 this.y.push(this.snakeHead.x);
-    } else {
+ this.snakeHead.x+=5;
+this.snakeHead.y+=5;
+this.food=0;  
+} else {
+    
         this.x.push(this.snakeHead.x);
 this.y.push(this.snakeHead.y);
-        this.snakeHead.x+=1;
-this.snakeHead.y+=1;
- this.clearEnd(this.snakeHead,this.x.pop(),this.y.pop());
-
+this.clearEnd(this.snakeHead,this.x.shift(),this.y.shift());
+        this.snakeHead.x+=5;
+this.snakeHead.y+=5;
+ 
+this.food=1;
 } 
-this.snakeCanvas.style.backgroundColor="green";
 //this.clearCanvas();
-this.snakeCanvas.style.backgroundColor="yellow";
    this.updateSnake(this.snakeHead,this.snakeHead.x,this.snakeHead.y);
-   this.snakeCanvas.style.backgroundColor="blue";
 }
 //pass in obj that contain x y size and color to have them rendered on canvas
 
@@ -46,18 +48,31 @@ updateSnake(obj,x,y){
     this.context.fillStyle=obj.color;
 this.context.fillRect(x,y,obj.size,obj.size);
 }
-//clears Snake canvas of objects
+
+//clears Snake end from canvas
 clearEnd(obj,x,y){
    // this.context.fillStyle="purple";
    // this.context.fillRect(x,y,obj.size,obj.size);
  this.context.clearRect(x, y, obj.size, obj.size);
 
 }
+//clears Snake canvas of objects
 clearCanvas(){
      this.context.clearRect(0, 0, this.snakeCanvas.width, this.snakeCanvas.height);
 
 }
+moveUp(){
 
+}
+moveDown(){
+
+}
+moveRight(){
+
+}
+moveLeft(){
+
+}
 }
 
  const game = new SnakeGame()
