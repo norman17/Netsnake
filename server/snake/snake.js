00e88abbc79279dd990ea2952deae75ed506f2a5
sklarqui <sklarqui@purdue.edu>
2018-04-14 19:51:24 -0400

    Got snake growth to work and have tail follow

--- server/snake/snake.js
+++ server/snake/snake.js
@@ -7,35 +7,48 @@ this.snakeCanvas = document.getElementById("snakeCanvas");
  this.direction=1;
  this.food=0;
 //this.context.fillRect(10,10,50,50);
-this.interval = setInterval(this.updateSnakeCanvas.bind(this),160);
-this.x=[0];
-this.y=[0];
+this.x=[];
+this.y=[];
+this.interval = setInterval(this.updateSnakeCanvas.bind(this),350);
+
 this.snakeHead = {
-    x: 25,
-    y: 25,
+    x: 0,
+    y: 0,
     size:5,
     color: "green",
     length: 1,
-    endx: 0,
-    endy: 0,
+    prevx: 0,
+    prevy: 0,
 }
 
 }
     //updates snakecanvas
 updateSnakeCanvas(){
-    console.log(this.x);
     this.x.push(this.snakeHead.x);
 this.y.push(this.snakeHead.y);
-        this.snakeHead.x+=5;
+this.snakeHead.prevx=this.snakeHead.x;
+this.snakeHead.prevy=this.snakeHead.y;        
+this.snakeHead.x+=0;
         this.snakeHead.y+=5;
-    if(this.food===1){
-this.food=0;  
-} else {    
- this.clearEnd(this.snakeHead,this.x.shift(),this.y.shift());
-this.food++;
-} 
-//this.clearCanvas();
-   this.updateSnake(this.snakeHead,this.snakeHead.x,this.snakeHead.y);
+   
+if(this.food>25){
+    if(this.x.length>0){
+        
+        this.clearEnd(this.snakeHead,this.x.shift(),this.y.shift());
+        this.updateSnake(this.snakeHead,this.snakeHead.x,this.snakeHead.y)
+        //this.x.push(this.snakeHead.x);
+        //this.y.push(this.snakeHead.y);
+    } else{
+this.clearEnd(this.snakeHead,this.x.pop(),this.y.pop());}
+} else{
+    this.updateSnake(this.snakeHead,this.snakeHead.x,this.snakeHead.y);
+   
+}
+this.food+=5;
+
+
+
+  
 }
 
 //pass in obj that contain x y size and color to have them rendered on canvas
