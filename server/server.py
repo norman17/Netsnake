811881947e60465e7a009678a49668d458ee7bce
norman17 <norman17@purdue.edu>
2018-04-16 17:19:45 -0400

    created scoreboard.js, there is mariadb code in the server that may not belong there. I also created the scoreboard.html file, that will be served with a link on the index.html page.

--- server/server.py
+++ server/server.py
@@ -1,24 +1,46 @@
 from flask import flask
 from flask import request
 from flask import render_template
+import mysql.connector as mariadb
+
+mdbconnection = mariadb.connect(user='netsnake', password='NETSNAKE', database='scoreboard')
+cursor = mdbconnection.cursor()
+
 
 @app.route('/', methods=['GET'])
 def giveIndex():
     #return html file for the index
-    return render_template('PATHTOFILEHERE', name=name)
-
-@app.route('/Netsnake', methods=['GET'])
-def giveSnake():
-    #return html file that has snake game in javascript
-    return render_template('PATHTOFILEHERE', name=name)
+    return render_template('index.html', name=name)
 
 @app.route('/scoreboard', methods=['GET'])
 def giveScoreboard():
+    mdbconnection = mariadb.connect(user='netsnake', password='NETSNAKE', database='scoreboard')
+    cursor = mdbconnection.cursor()
+    try:
+        cursor.execute("SELECT username,score FROM scores ORDER BY scores DESC")
+    except mariadb.Error as error:
+        print ("Error: {}".format(error))
     #return html file that has scoreboard
-    return render_template('PATHTOFILEHERE', name=name)
+    i = 0
+    sortedScores = []
+    for username, score in cursor:
+        sortedScores.append((username))
+        if i == 10:
+            break
+
+
+    mdbconnection.close()
+    return render_template('scoreboard.html', name=name)
 
 @app.route('/postscore', methods=['POST'])
 def postScore():
+    mdbconnection = mariadb.connect(user='netsnake', password='NETSNAKE', database='scoreboard')
+    cursor = mdbconnection.cursor()
     #takes the score sent in the route (RESTful API) and stores it in MongoDB
     score = request.args.get('score')
-    return render_template('PATHTOFILEHERE', name=name)
+    try:
+        cursor.execute("INSERT INTO scores (username,score) VALUES (%s,%f)", (username,score))
+    except mariadb.Error as error:
+        print ("Error: {}".format(error))
+    mdbconnection.close()
+    return
