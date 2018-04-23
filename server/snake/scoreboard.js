5b8dfbb8d6cff1e52a4554b5ef4dd0b20b3cb3ed
norman17 <norman17@purdue.edu>
2018-04-23 15:37:48 -0400

    search scores  button SHOULD be working but MAY NOT.

--- server/snake/scoreboard.js
+++ server/snake/scoreboard.js
@@ -1,1 +1,20 @@
 
+
+    function getUserScores() {
+       // if(justPost===false){
+            var username = document.getElementById("username").value;
+            fetch('/player/' + username, { method: 'GET' })
+            /*
+            var xml = new XMLHttpRequest();
+            xml.onreadystatechange = function() {
+                if (xml.readyState == 4 && xml.status == 200) {
+                    callback(xml.responseText);
+                }
+            }
+            var username = document.getElementById("username").value;
+            xml.open("POST", '/postscore?score=' + game.score.toString() + '&username=' + username, false); //NEED TO FIND URL
+            xml.send(null);
+        */
+    //justPost=true;
+    //}
+        }
